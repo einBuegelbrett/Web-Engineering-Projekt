3ba52d6f5c1b389fe64a21f43f74edc5df9e2b5b
Sendke <sven.sendke@allianz.de>
2023-06-21 10:06:40 +0200

    Neue Initialisierung

---
+++ Sven Sendke Aufgaben/woche5/github_client/script.js
@@ -0,0 +1,37 @@
+const token = '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';
+
+function fetchButton() {
+    const input = document.getElementById('password').value;
+    console.log(input);
+    fetch('https://api.github.com/user/repos', {
+        headers: {
+            'Authorization': 'Bearer ' + input
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Fehler :(');
+        }
+    })
+    .then(data => {
+        const reposList = document.getElementById('repos-list');
+        reposList.innerHTML = '';
+        data.forEach(repo => {
+            const listItem = document.createElement('li');
+            listItem.textContent = repo.name;
+            reposList.appendChild(listItem);
+        });
+        document.getElementById('message').textContent = 'Es hat geklappt :)';
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById('message').textContent = 'Fehler: ' + error.message;
+    });
+
+    //.then(data => console.log(data))
+    //.catch(error => console.log(error));
+}
+
+addEventListener('submit', fetchButton());
