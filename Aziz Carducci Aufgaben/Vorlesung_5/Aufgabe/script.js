a525d694f0c8b4a2111217d2a5dd9367e6ee5b81
Carducci <aziz.carducci@allianz.de>
2023-07-07 14:12:53 +0200

    Vorlesung 5 weitergearbeitet, noch nicht ganz fertig, fetch datei wird nicht richtig gelesen

--- Aziz Carducci Aufgaben/Vorlesung_5/Aufgabe/script.js
+++ Aziz Carducci Aufgaben/Vorlesung_5/Aufgabe/script.js
@@ -1,14 +1,39 @@
-const reposlist = document.getElementById('repos-list');
-const loginform = document.getElementById('login-form');
+// script.js
 
 function getToken() {
-    fetch('https://api.github.com/user/repos' , {
-        headers: {
-        'Authorization': 'Bearer' + loginform.innerText
-        }
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+  const token = document.getElementById('password').value;
+
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      'Authorization': 'Bearer ' + token
+  }
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } 
+  })
+
+    .then(data => {
+      displayRepositories(data);
+      document.getElementById('message').textContent = 'Success';
+    })
+    .catch(error => {
+      document.getElementById('message').textContent = error.message;
+    });
 }
+
+function displayRepositories(repositories) {
+  const reposList = document.getElementById('repos-list');
+  reposList.innerHTML = '';
+
+  repositories.forEach(repo => {
+    const listItem = document.createElement('li');
+    listItem.textContent = repo.full_name;
+    reposList.appendChild(listItem);
+  });
+}
+
+addEventListener('submit', getToken());
+
 
